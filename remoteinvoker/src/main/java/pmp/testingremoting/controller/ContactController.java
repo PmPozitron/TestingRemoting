c9063c598e0854a1b54bb08e848a6554b73fc008
Postmaster Pozitron <pmpozitron@gmail.com>
2016-09-18 21:17:17 +0400

    Added @RestController.

--- remoteinvoker/src/main/java/pmp/testingremoting/controller/ContactController.java
+++ remoteinvoker/src/main/java/pmp/testingremoting/controller/ContactController.java
@@ -1,7 +1,13 @@
 package pmp.testingremoting.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import pmp.testingremoting.service.ContactService;
+
+import javax.annotation.Resource;
 
 /**
  * Created by Pozitron on 18.09.2016.
@@ -12,4 +18,12 @@ public class ContactController {
 
     public static final String QUALIFIER = "contactController";
     public static final String MAPPING = "/contact";
+
+    @Resource
+    private ContactService service;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<String> findAll() {
+        return new ResponseEntity<String>(service.findAll().toString(), HttpStatus.OK);
+    }
 }
