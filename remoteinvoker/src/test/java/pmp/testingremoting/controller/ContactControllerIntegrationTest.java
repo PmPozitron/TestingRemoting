49726e3a3526b9aa61608e349efd462fb3a9c098
Postmaster Pozitron <pmpozitron@gmail.com>
2017-10-21 03:23:20 +0300

    contact controller integration test works in manual mode (needs remote service to be up)

---
+++ remoteinvoker/src/test/java/pmp/testingremoting/controller/ContactControllerIntegrationTest.java
@@ -0,0 +1,64 @@
+package pmp.testingremoting.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.remoting.httpinvoker.HttpInvokerProxyFactoryBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import pmp.testingremoting.service.ContactService;
+
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by Pozitron on 18.09.2016.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {
+        ContactControllerIntegrationTest.TestConfig.class,
+})
+@WebAppConfiguration
+public class ContactControllerIntegrationTest {
+
+    @Autowired
+    private ContactController controller;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void initMockMvc() {
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void thatData_isAccessibleRemotely() throws Exception {
+        assertNotNull("Contact controller was not wired !", controller);
+
+        ResultActions ra = mockMvc.perform(get(ContactController.MAPPING));
+        ra.andExpect(status().isOk());
+    }
+
+    @Configuration
+    @ImportResource(locations = {
+            "classpath*:spring/remoteInvokerContext.xml",
+            "classpath*:spring/webContext.xml",
+    })
+    static class TestConfig {
+
+    }
+}
